f904966216a0301355713e52a3475b1f939219ef
Marcos Blankenburg Dantas <hunterbd@hotmail.com>
2023-04-06 09:31:19 -0300

    Atribuindo o método Sets.

--- AppListaCurso/app/src/main/java/devandroid/mbd/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/mbd/applistacurso/view/MainActivity.java
@@ -8,6 +8,7 @@ import devandroid.mbd.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+
+        // Atribuir conteúdo, dados, valores para o objeto
+        // Conforme o seu modelo, template
+        pessoa.setPrimeiroNome("Marcos");
+        pessoa.setSobreNome("Dantas");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("16-9999-9999");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Marta");
+        outraPessoa.setSobreNome("Dantas");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("16-8888-8888");
+
+        int parada=0;
     }
 }
